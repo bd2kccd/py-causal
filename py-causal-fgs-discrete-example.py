3b631c67f220ff9bd7826f447f93f196311625ad
chirayukong <chirayu.kong@gmail.com>
2016-07-14 14:10:37 -0400

    Updated python examples

--- py-causal-fgs-discrete-example.py
+++ py-causal-fgs-discrete-example.py
@@ -9,11 +9,15 @@ from IPython.display import SVG
 data_dir = os.path.join(os.getcwd(), 'data', 'audiology.txt')
 df = pd.read_table(data_dir, sep="\t")
 
+from pycausal import pycausal as pc
+
+pc.start_vm(java_max_heap_size = '100M')
+
 from pycausal import search as s
 
 fgs = s.fgsDiscrete(df,structurePrior = 1.0, samplePrior = 1.0, 
-                            depth = 3, faithfulness = True, numOfThreads = 2, 
-                            verbose = True, java_max_heap_size = '500M')
+                            depth = 3, heuristicSpeedup = True, numOfThreads = 2, 
+                            verbose = True)
 
 fgs.getNodes()
 fgs.getEdges()
@@ -25,3 +29,4 @@ f = open('fgs-discrete.dot','w')
 f.write(svg_str)
 f.close()
 
+pc.stop_vm()
