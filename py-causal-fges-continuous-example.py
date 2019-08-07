66a5f4ce25fe16596cdd3c9b989c1df77c10bca0
chirayukong <chirayu.kong@gmail.com>
2019-08-07 17:45:21 -0400

    Update py-causal-fges-continuous-example.py

--- py-causal-fges-continuous-example.py
+++ py-causal-fges-continuous-example.py
@@ -17,10 +17,17 @@ tetrad = s.tetradrunner()
 tetrad.run(algoId = 'fges', dfs = df, scoreId = 'sem-bic-score', dataType = 'continuous',
            maxDegree = -1, faithfulnessAssumed = True, verbose = True)
 
-tetrad.getNodes()
-tetrad.getEdges()
+print(tetrad.getNodes())
+print(tetrad.getEdges())
 
-dot_str = pc.tetradGraphToDot(tetrad.getTetradGraph())
+graph = tetrad.getTetradGraph()
+print('Graph BIC: {}'.format(graph.getAttribute('BIC')))
+nodes = graph.getNodes()
+for i in range(nodes.size()):
+    node = nodes.get(i)
+    print('Node {} BIC: {}'.format(node.getName(),node.getAttribute('BIC')))
+
+dot_str = pc.tetradGraphToDot(graph)
 graphs = pydot.graph_from_dot_data(dot_str)
 graphs[0].write_svg('fges-continuous.svg')
 
